4947b415a34ee031ff89d15a05929c39d114b5c2
Sottie1 <sottiegason@gmail.com>
2023-06-13 21:19:54 -1200

    [update]

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function groupNames() {
-    var namesInput = document.getElementById("nameInput").value;
-    var names = namesInput.split(",");
-    var groupSize = parseInt(document.getElementById("groupSizeInput").value);
+  var namesInput = document.getElementById("nameInput").value;
+  var names = namesInput.split(",");
+  var groupSize = parseInt(document.getElementById("groupSizeInput").value);
+  var groupDescription = document.getElementById("groupDescriptionInput").value;
 
-    var groups = groupNamesHelper(names, groupSize);
-    displayGroups(groups);
-  }
+  var groups = groupNamesHelper(names, groupSize);
+  displayGroups(groups, groupDescription);
+}
 
   function groupNamesHelper(names, groupSize) {
     shuffle(names);  // Randomize the order of names
@@ -42,105 +43,45 @@ function groupNames() {
     return array;
   }
 
-  function displayGroups(groups) {
-    var outputDiv = document.getElementById("output");
-    outputDiv.innerHTML = "";
-
-    for (var i = 0; i < groups.length; i++) {
-      var groupDiv = document.createElement("div");
-      groupDiv.className = "card";
-
-      var groupHeading = document.createElement("h3");
-      groupHeading.textContent = "Group " + (i + 1);
-      groupDiv.appendChild(groupHeading);
-
-      var groupList = document.createElement("ul");
-
-      for (var j = 0; j < groups[i].length; j++) {
-        var nameItem = document.createElement("li");
-        nameItem.textContent = groups[i][j];
-        groupList.appendChild(nameItem);
-      }
-
-      groupDiv.appendChild(groupList);
-      outputDiv.appendChild(groupDiv);
-    }
-  }
-
-  function exportPDF() {
-    var groups = getGroupsArray();
-
-    var doc = new jsPDF();
-    var y = 10;
-
-    for (var i = 0; i < groups.length; i++) {
-      doc.setFontSize(16);
-      doc.setTextColor("#663de4");
-      doc.text("Group " + (i + 1), 10, y);
-
-      y += 10;
-
-      doc.setFontSize(12);
-      doc.setTextColor("#001019");
-
-      for (var j = 0; j < groups[i].length; j++) {
-        doc.text(groups[i][j], 10, y);
-        y += 10;
-      }
-
-      y += 10;
-    }
-
-    doc.save("groups.pdf");
-  }
-
-function exportWord() {
-  var groups = groupNamesHelper(names, groupSize);
-  var content = generateGroupContent(groups);
-  var filename = "groups.docx";
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://1drv.ms/w/s!AoA4ig7H7EN3gVTiiYgsEzRh_7Au?e=g0SrOy", true);
-  xhr.responseType = "arraybuffer";
-
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      var data = new Uint8Array(xhr.response);
-      var doc = new window.docxtemplater().loadZip(new JSZip(data));
-
-      doc.setData({
-        groups: content
-      });
-
-      try {
-        doc.render();
-      } catch (error) {
-        console.log(error);
-        return;
-      }
-
-      var out = doc.getZip().generate({
-        type: "blob"
-      });
-
-      saveAs(out, filename);
-    }
-  };
-
-  xhr.send();
-}
-
-function generateGroupContent(groups) {
-  var content = [];
+  function displayGroups(groups, groupDescription) {
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = "";
 
   for (var i = 0; i < groups.length; i++) {
-    var group = {
-      number: i + 1,
-      names: groups[i]
-    };
+    var groupDiv = document.createElement("div");
+    groupDiv.className = "card";
 
-    content.push(group);
+    var groupHeading = document.createElement("h3");
+    groupHeading.textContent = "Group " + (i + 1);
+    groupDiv.appendChild(groupHeading);
+
+    var descriptionParagraph = document.createElement("p");
+    descriptionParagraph.textContent = groupDescription;
+    descriptionParagraph.className = "group-description"; // Apply the new CSS class
+    groupDiv.appendChild(descriptionParagraph);
+
+    var groupList = document.createElement("ul");
+
+    for (var j = 0; j < groups[i].length; j++) {
+      var nameItem = document.createElement("li");
+      nameItem.textContent = groups[i][j];
+      groupList.appendChild(nameItem);
+    }
+
+    groupDiv.appendChild(groupList);
+    outputDiv.appendChild(groupDiv);
   }
+}
 
-  return content;
+
+function printGroups() {
+  var outputDiv = document.getElementById("output");
+  var printContent = outputDiv.innerHTML;
+  var printWindow = window.open('', '_blank');
+  printWindow.document.open();
+  printWindow.document.write('<html><head><title> Groups</title></head><body>');
+  printWindow.document.write(printContent);
+  printWindow.document.write('</body></html>');
+  printWindow.document.close();
+  printWindow.print();
 }
